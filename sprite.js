e416f14a819ccbe73471daf764bf4778308d8bdc
Jake Nelken <jnelks@gmail.com>
2016-02-14 00:09:10 -0500

    Change bird to samoyed.

--- sprite.js
+++ sprite.js
@@ -1,4 +1,4 @@
-var 
+var
 
 s_bird,
 s_bg,
@@ -19,7 +19,7 @@ function Sprite(img, x, y, width, height) {
 	this.y = y*2;
 	this.width = width*2;
 	this.height = height*2;
-};
+}
 Sprite.prototype.draw = function(ctx, x, y) {
 	ctx.drawImage(this.img, this.x, this.y, this.width, this.height,
 		x, y, this.width, this.height);
@@ -27,24 +27,38 @@ Sprite.prototype.draw = function(ctx, x, y) {
 
 function initSprites(img) {
 
+	var samoyed_img1 = new Image();
+	samoyed_img1.src = "res/samoyed1.png";
+	var samoyed_img2 = new Image();
+	samoyed_img2.src = "res/samoyed2.png";
+	var samoyed_img3 = new Image();
+	samoyed_img3.src = "res/samoyed3.png";
+	var samoyed_img4 = new Image();
+	samoyed_img4.src = "res/samoyed4.png";
+
 	s_bird = [
-		new Sprite(img, 156, 115, 17, 12),
-		new Sprite(img, 156, 128, 17, 12),
-		new Sprite(img, 156, 141, 17, 12)
+		// new Sprite(img, 156, 115, 17, 12),
+		// new Sprite(img, 156, 128, 17, 12),
+		// new Sprite(img, 156, 141, 17, 12)
+		new Sprite(samoyed_img1, 0, 0, 64, 64),
+		new Sprite(samoyed_img2, 0, 0, 64, 64),
+		new Sprite(samoyed_img3, 0, 0, 64, 64),
+		new Sprite(samoyed_img4, 0, 0, 64, 64)
 	];
-	
+
+
 	s_bg = new Sprite(img,   0, 0, 138, 114);
 	s_bg.color = "#70C5CF";
 	s_fg = new Sprite(img, 138, 0, 112,  56);
-	
+
 	s_pipeNorth = new Sprite(img, 251, 0, 26, 200);
 	s_pipeSouth = new Sprite(img, 277, 0, 26, 200);
-	
+
 	s_text = {
 		FlappyBird: new Sprite(img, 59, 114, 96, 22),
 		GameOver:   new Sprite(img, 59, 136, 94, 19),
 		GetReady:   new Sprite(img, 59, 155, 87, 22)
-	}
+	};
 	s_buttons = {
 		Rate:  new Sprite(img,  79, 177, 40, 14),
 		Menu:  new Sprite(img, 119, 177, 40, 14),
@@ -52,7 +66,7 @@ function initSprites(img) {
 		Score: new Sprite(img,  79, 191, 40, 14),
 		Ok:    new Sprite(img, 119, 191, 40, 14),
 		Start: new Sprite(img, 159, 191, 40, 14)
-	}
+	};
 
 	s_score  = new Sprite(img, 138,  56, 113, 58);
 	s_splash = new Sprite(img,   0, 114,  59, 49);
@@ -66,8 +80,8 @@ function initSprites(img) {
 		for (var i = 0, len = num.length; i < len; i++) {
 			var n = parseInt(num[i]);
 			ctx.drawImage(img, step*n, this.y, this.width, this.height,
-				x, y, this.width, this.height)
+				x, y, this.width, this.height);
 			x += step;
 		}
-	}
+	};
 }
